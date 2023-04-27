1488df25408a0a7c0fcf6622a402cf695f33e5d2
Will Paceley <pace0033@algonquinlive.com>
2023-04-27 16:35:33 -0400

    Initial Commit

---
+++ MarsWeather/MarsWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarsWeatherApp.swift
+//  MarsWeather
+//
+//  Created by Will Paceley on 2023-04-27.
+//
+
+import SwiftUI
+
+@main
+struct MarsWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
